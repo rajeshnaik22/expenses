2a70c6ed03a7740e60226fdbd969fd6568128769
rajeshnaik22 <rajesh.naik22@gmail.com>
2022-07-21 20:14:53 +0530

    UPTO Users index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  include Roles
+  
   # TODO: enable confirmable when mail sending available
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable, :trackable
 
   has_paper_trail
+
+
+
+
+
 end
