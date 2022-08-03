96cf17ef3b6808014283335c6ceb2db79b92856c
rajeshnaik22 <rajesh.naik22@gmail.com>
2022-08-03 22:40:13 +0530

    ffmpeg settings

--- app/services/media.rb
+++ app/services/media.rb
@@ -2,7 +2,7 @@ class Media
     class << self
         def trim(source_path, destination_path, width: -1, height: -1)
             puts "ffmpeg -i #{source_path} -vf scale=#{width}:#{height} #{destination_path}"
-            `ffmpeg -i #{source_path} -vf scale=#{width}:#{height} #{destination_path}`
+            `ffmpeg -y -loglevel warning -hide_banner -stats  -i #{source_path} -vf scale=#{width}:#{height} #{destination_path}`
         end
     end
 
